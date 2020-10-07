b46128b31704269efaf088a7235415be7e75d5cd
Atsushi Ishida <gipcompany@gmail.com>
2020-10-07 22:29:53 +0900

    create setName

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SignIn from './SignIn';
 
 export default () => {
-  return <SignIn />;
+  const [name, setName] = useState('');
+  console.log({ name });
+
+  return <SignIn setName={setName} />;
 };
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -44,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+export default function SignIn({ setName }) {
   const classes = useStyles();
 
   return (
