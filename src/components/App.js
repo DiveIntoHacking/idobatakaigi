dad1b2a078bcdb2aad2275164a71fcc8c2c17308
Atsushi Ishida <gipcompany@gmail.com>
2020-10-10 02:58:45 +0900

    save name in japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 
+import Main from './Main';
 import SignIn from './SignIn';
 
 export default () => {
   const [name, setName] = useState('');
   console.log({ name });
 
-  return <SignIn setName={setName} />;
+  if (name === '') {
+    return <SignIn setName={setName} />;
+  } else {
+    return <Main name={name} />;
+  }
 };
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
-  console.log({ disabled, string });
+  const [isComposed, setIsComposed] = useState(false);
 
   useEffect(() => {
     const disabled = string === '';
@@ -74,11 +74,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isComposed) return;
+
               if (e.key === 'Enter') {
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
