994b456a6f4cf66244a34d38788aba34e1f77879
Atsushi Ishida <gipcompany@gmail.com>
2020-10-10 11:31:46 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 
 import Main from './Main';
 import SignIn from './SignIn';
+import config from '../config.json';
 
 export default () => {
   const [name, setName] = useState('');
-  console.log({ name });
 
-  if (name === '') {
+  if (config.signInEnabled && name === '') {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
