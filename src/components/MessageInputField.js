6747075c35245e81b207a03c4ce9bc5bfc785ce6
Atsushi Ishida <gipcompany@gmail.com>
2020-10-10 15:06:39 +0900

    apply grid-row

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+  },
+});
 
 const MessageInputField = () => {
-  return <div>MessageInputField</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>MessageInputField</div>;
 };
 
 export default MessageInputField;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,
+  },
+});
 
 const MessageList = () => {
-  return <div>MessageList</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>MessageList</div>;
 };
 
 export default MessageList;
