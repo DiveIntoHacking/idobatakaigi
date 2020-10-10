e1bdff1bfd60c3449baa45cc26ee84adabf626eb
Atsushi Ishida <gipcompany@gmail.com>
2020-10-11 02:21:03 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBXsfqls4q8Pkg3UY04l03KmG_Iu9IhOcc',
-  authDomain: 'idobatakaigi-with-ham.firebaseapp.com',
-  databaseURL: 'https://idobatakaigi-with-ham.firebaseio.com',
-  projectId: 'idobatakaigi-with-ham',
-  storageBucket: 'idobatakaigi-with-ham.appspot.com',
-  messagingSenderId: '979582252050',
-  appId: '1:979582252050:web:84d437a2c2a7c6706ed4cb',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
