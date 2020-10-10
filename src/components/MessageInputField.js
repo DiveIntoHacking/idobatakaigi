08fd78ff73396af51ac4b3e43aaddea002ee7363
Atsushi Ishida <gipcompany@gmail.com>
2020-10-11 01:50:23 +0900

    set grid item

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -20,13 +20,15 @@ const MessageInputField = ({ name }) => {
   return (
     <div className={classes.root}>
       <Grid container>
-        <Grid xs={1}>
+        <Grid item xs={1}>
           <Avatar src={avatarPath} />
         </Grid>
-        <Grid xs={10}>
+        <Grid item xs={10}>
           <MessageField name={name} setText={setText} text={text} />
         </Grid>
-        <Grid xs={1}>ボタン</Grid>
+        <Grid item xs={1}>
+          ボタン
+        </Grid>
       </Grid>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -12,8 +12,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = ({ name, text }) => {
-  messageRef.push({ name, text });
+  messagesRef.push({ name, text });
 };
