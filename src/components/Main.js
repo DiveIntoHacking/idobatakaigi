de1f367ca207113a2a7800a68f9fa82188334ad8
kenklum <kenkondolumbreras@gmail.com>
2023-03-15 10:58:25 +0900

    test commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import MessageInputField from './MessageInputField';
-import MessageList from './MessageList';
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
 
 const useStyles = makeStyles({
   root: {
-    display: 'grid',
-    height: '100vh',
-    gridTemplateRows: '1fr auto',
+    display: "grid",
+    height: "100vh",
+    gridTemplateRows: "1fr auto",
   },
 });
 
@@ -18,6 +18,7 @@ const Main = ({ name }) => {
   return (
     <div className={classes.root}>
       <MessageList />
+      hello world
       <MessageInputField name={name} />
     </div>
   );
